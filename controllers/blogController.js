97f20d35e4e07d813e69a5761b1373ae471ff6f6
Emmanuel Zambrano <ezambran@hawk.iit.edu>
2017-10-18 01:04:09 -0500

    edited view files for consistancy

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,17 +1,16 @@
-//var Blog = require('../models/blog');
-var Blog = new Object;
+var Blog = require('../models/blog');
 
 var async = require('async');
 
 exports.blog_create_get = function(req, res, next){
-  res.render('blog_create_get', { title: 'New Blog'});
+  res.render('blog_create', { title: 'New Blog'});
 };
 
 exports.blog_create_post = function(req, res, next){
   //put data into database
   var blogTitle = req.body.title;
   var blogMessage = req.body.message;
-  res.render('blog_create_post', { title: 'Blog Posted', blogTitle: blogTitle, blogMessage: blogMessage});
+  res.render('blog_create', { title: 'Blog Posted', blogTitle: blogTitle, blogMessage: blogMessage});
 };
 
 exports.blog_delete_get = function(req, res, next){
@@ -31,5 +30,10 @@ exports.blog_update_post = function(req, res, next){
 };
 
 exports.blog_list = function(req, res){
-  res.render('blog_list', { title: 'jacefromthefuture', blog_list: Blog });
+  Blog.find({}, 'title body').exec(function (err, list_blogs) {
+     if (err) {
+       return next(err);
+     }
+     res.render('blog_list', { title: 'Blogs', blog_list: list_blogs });
+   });
 };
