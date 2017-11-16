fead096b731c0a0e14baa20b36103ae9bc9feec0
Emmanuel Zambrano <ezambran@hawk.iit.edu>
2017-11-15 22:48:44 -0600

    updated blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,92 +1,31 @@
-var Blog = require('../models/blog');
+var request = require('request');
 
 exports.blog_list = function(req, res){
-  Blog.find({}, 'title date').exec(function (err, list_blogs) {
-     if (err) {
-       return next(err);
-     }
-     res.render('blog_list', { title: 'Blogs', blog_list: list_blogs });
-   });
+  var options = { method: 'GET',
+    url: 'http://localhost:3000/api'
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    var blogs = JSON.parse(body);
+    console.log(blogs);
+    res.render('blogs', { title: 'Blogs', blog_list: blogs });
+  });
 };
 
 exports.blog_view = function(req, res, next){
-  Blog.findById(req.params.id).exec(function (err, blog){
-    if(err){
-      return next(err);
-    }
-    res.render('blog_view', { title: 'Blogs', blog: blog});
-  });
-};
-
-exports.blog_create_get = function(req, res, next){
-  res.render('blog_create', { title: 'New Blog'});
-};
-
-exports.blog_create_post = function(req, res, next){
-  //actions to perform with post data
-  req.checkBody('title', 'Title must not be empty.').notEmpty();
-  req.checkBody('message', 'Summary must not be empt').notEmpty();
-
-  req.sanitize('title').escape();
-  req.sanitize('message').escape();
-  req.sanitize('title').trim();
-  req.sanitize('message').trim();
-
-  var blog = new Blog(
-    { title: req.body.title,
-      message: req.body.message
-    });
-
-  var errors = req.validationErrors();
-  if (errors) {
-    res.render('blog_create', { title: 'New Blog', errorMessage: 'Fill in the information'});
-  }
-  else {
-    //save data to database
-    blog.save(function (err) {
-      if (err) {
-        console.log(err)
-      }
+  var url = 'http://localhost:3000/api/' + req.params.id
+  var options = { method: 'GET',
+    url: url
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+    if(body == 'null'){
       res.redirect('/blogs');
-    });
-  }
-};
-
-exports.blog_update_get = function(req, res, next){
-  Blog.findById(req.params.id).exec(function (err, blog){
-    if(err){
-      return next(err);
     }
-    res.render('blog_update', { title: 'Update Blog', blog: blog});
+    else {
+      var blog = JSON.parse(body);
+      res.render('blog_view', {blog : blog});
+    }
   });
-};
-
-exports.blog_update_post = function(req, res, next){
-  //actions to perform with post data
-  req.checkBody('title', 'Title must not be empty.').notEmpty();
-  req.checkBody('message', 'Summary must not be empt').notEmpty();
-
-  req.sanitize('title').escape();
-  req.sanitize('message').escape();
-  req.sanitize('title').trim();
-  req.sanitize('message').trim();
-
-  var blog = new Blog(
-    { title: req.body.title,
-      message: req.body.message,
-      _id:req.params.id
-    });
-
-  var errors = req.validationErrors();
-  if (errors) {
-    res.render('blog_update', { title: 'Update Blog'});
-  }
-  else {
-    Blog.findOneAndUpdate({_id: req.params.id}, blog, {new: true}, function(err, blog) {
-      if (err){
-        res.send(err);
-      }
-      res.redirect('/blogs');
-    });
-  }
 };
--- routes/blogs.js
+++ routes/blogs.js
@@ -3,14 +3,6 @@ var router = express.Router();
 
 var blog_controller = require('../controllers/blogController');
 
-router.get('/create', blog_controller.blog_create_get);
-
-router.post('/create', blog_controller.blog_create_post);
-
-router.get('/:id/update', blog_controller.blog_update_get);
-
-router.post('/:id/update', blog_controller.blog_update_post);
-
 router.get('/:id', blog_controller.blog_view);
 
 router.get('/', blog_controller.blog_list);
